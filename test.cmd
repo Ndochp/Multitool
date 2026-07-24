8bb63461cbf03adfcc6a631e200bbd3cfb45ac6f
Ndochp <ndochp@gmail.com>
2026-07-24 23:51:57 +0300

    +1

--- test.cmd
+++ test.cmd
@@ -1,2 +1,7 @@
 @chcp 65001
-call vrunner vanessa %*
+echo %time%
+call UPdate.cmd
+echo %time%
+"D:\Soft\1Cv8\8.3.27.2074\bin\1cv8c.exe" /IBConnectionString "File=""D:\GIT_REPO\Multitool\build\ib"";" /C "RunUnitTests=D:\GIT_REPO\Multitool\tests\yaxUnit.json"
+echo %time%
+@REM call vrunner vanessa %*
--- update.cmd
+++ update.cmd
@@ -1,7 +1,7 @@
 @chcp 65001
 
 @rem обновление конфигурации основной разработческой ИБ без поддержки или на поддержке. по умолчанию в каталоге build/ib
-call vrunner update-dev --src src/cf --disable-support
+@REM call vrunner update-dev --src src/cf --disable-support
 
 @rem обновление конфигурации основной разработческой ИБ из хранилища. для включения раскомментируйте код ниже
 @rem call vrunner loadrepo %*
@@ -12,7 +12,7 @@ call vrunner update-dev --src src/cf --disable-support
 @rem call vrunner compileepf src/erf/МойВнешнийОтчет build %*
 
 @rem собрать расширения конфигурации внутри ИБ
-@rem call vrunner compileext src/cfe/МоеРасширение МоеРасширение %*
-
+call vrunner compileext tests\cfe.test test %*
+call vrunner updateext test
 @rem обновление в режиме Предприятие
-call vrunner run --command "ЗапуститьОбновлениеИнформационнойБазы;ЗавершитьРаботуСистемы;" --execute $runnerRoot\epf\ЗакрытьПредприятие.epf %*
+@REM call vrunner run --command "ЗапуститьОбновлениеИнформационнойБазы;ЗавершитьРаботуСистемы;" --execute $runnerRoot\epf\ЗакрытьПредприятие.epf %*
